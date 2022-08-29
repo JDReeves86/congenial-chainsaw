eb7fe67d428e1e0f68fc3a6a1f66198840b9446e
JDReeves86 <Reeves.Jacobd@gmail.com>
2022-08-29 12:27:55 -0700

    initial commit

---
+++ Develop/index.js
@@ -0,0 +1,24 @@
+// TODO: Include packages needed for this application
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown')
+
+// TODO: Create an array of questions for user input
+const questions = [
+    'What is the title of your project?',
+    'Write a brief description of your application(s).',
+    'How does a user install your application(s)?',
+    'How does  auser use your application(s)?',
+    'Please provide instructions for contribution.',
+    'How would a contributer test their addition(s)/edit(s)?',
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+
+}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
